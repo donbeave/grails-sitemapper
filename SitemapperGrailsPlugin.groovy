0147237360dbc4a4c7039e0db943ab961b31641d
Alexey Zhokhov <donbeave@gmail.com>
2015-06-14 16:20:44 +0800

    Version bumped to 0.8.2.

--- SitemapperGrailsPlugin.groovy
+++ SitemapperGrailsPlugin.groovy
@@ -27,7 +27,7 @@ import org.apache.http.impl.client.DefaultHttpClient
  */
 class SitemapperGrailsPlugin {
 
-    def version = '0.8.2-SNAPSHOT'
+    def version = '0.8.2'
     def grailsVersion = '2.0 > *'
     def pluginExcludes = [
             'lib/**',
