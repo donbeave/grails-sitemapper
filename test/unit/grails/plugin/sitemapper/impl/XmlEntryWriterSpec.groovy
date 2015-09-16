e8c9446bd1c0663dbdb47e0ef627782e80bd72c6
Alexey Zhokhov <donbeave@gmail.com>
2015-09-16 13:15:58 +0800

    Added tests for mobile and alternate link extensions.

--- test/unit/grails/plugin/sitemapper/impl/XmlEntryWriterSpec.groovy
+++ test/unit/grails/plugin/sitemapper/impl/XmlEntryWriterSpec.groovy
@@ -1,6 +1,8 @@
 package grails.plugin.sitemapper.impl
 
 import grails.plugin.sitemapper.Entry
+import grails.plugin.sitemapper.extension.AlternateLink
+import grails.plugin.sitemapper.extension.Mobile
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.Specification
@@ -33,17 +35,45 @@ class XmlEntryWriterSpec extends Specification {
     def "Check entry object"() {
         given:
         Appendable output = new StringWriter()
-        XmlEntryWriter entryWriter = new XmlEntryWriter(output, "http://developer-b.com");
+        XmlEntryWriter entryWriter = new XmlEntryWriter(output, "http://t.com")
 
         when:
-        entryWriter.add(new Entry(location: location))
+        entryWriter.add(new Entry(data))
 
         then:
         output.toString().equals(expected)
 
         where:
-        tagName | location            | expected
-        "loc"   | 'http://test.com/1' | "<url><loc>http://test.com/1</loc></url>"
+        data                                            | expected
+        [location: 'http://test.com/about']             | '<url><loc>http://test.com/about</loc></url>'
+
+        [location  : '/uri',
+         modifiedAt: new Date(1442378298922)]           | '<url><loc>http://t.com/uri</loc><lastmod>2015-09-16T04:38:18+00:00</lastmod></url>'
+
+        [location       : '/uri',
+         changeFrequency: 'ALWAYS']                     | '<url><loc>http://t.com/uri</loc><changefreq>always</changefreq></url>'
+
+        [location: '/uri',
+         priority: 0]                                   | '<url><loc>http://t.com/uri</loc><priority>0.0</priority></url>'
+
+        [location: '/uri',
+         priority: 0.7]                                 | '<url><loc>http://t.com/uri</loc><priority>0.7</priority></url>'
+
+        [location: '/uri',
+         expires : new Date(1442378756773)]             | '<url><loc>http://t.com/uri</loc><expires>2015-09-16T04:45:56+00:00</expires></url>'
+
+        [location  : '/uri',
+         extensions: [new Mobile()]]                    | '<url><loc>http://t.com/uri</loc><mobile:mobile/></url>'
+
+        [location  : '/uri',
+         extensions: [
+                 new AlternateLink(location: '/test')]] | '<url><loc>http://t.com/uri</loc><xhtml:link rel="alternate" href="http://t.com/test"/></url>'
+
+        [location  : '/uri',
+         extensions: [new AlternateLink(
+                 location: '/test',
+                 language: 'ru-ru'
+         )]]                                            | '<url><loc>http://t.com/uri</loc><xhtml:link rel="alternate" hreflang="ru-ru" href="http://t.com/test"/></url>'
     }
 
     /*
